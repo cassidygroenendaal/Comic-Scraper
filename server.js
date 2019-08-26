5d6b7304b155d82e7a0f23b99afc966e3455bbfa
Cassidy G <cassidygroenendaal@gmail.com>
2019-08-26 15:11:37 -0400

    Add basic files to project

--- server.js
+++ server.js
@@ -1,2 +1,37 @@
+//======================================
+// CONFIGS
+//--------------------------------------
+
 const express = require("express"),
-  mongoose = require("mongoose");
+  exphbs = require("express-handlebars"),
+  mongoose = require("mongoose"),
+  logger = require("morgan"),
+  axios = require("axios"),
+  cheerio = require("cheerio");
+
+const app = express(),
+  PORT = process.env.PORT || 8080;
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+mongoose.connect("mongodb://localhost/scraper_db", { useNewUrlParser: true });
+
+//======================================
+// ROUTES
+//--------------------------------------
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+//======================================
+// SERVER START
+//--------------------------------------
+
+app.listen(PORT, () => console.log(`==> Server running on ${PORT}`));
